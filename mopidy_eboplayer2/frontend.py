2ccf68636be8d8b93107a78ecb27924d713eedbf
Erik Bongers <mail@erikbongers.net>
2025-10-11 22:07:19 +0200

    Copy to player2: 0d987b919ae35e30d27ee57d1428ca71310200f2

--- mopidy_eboplayer2/frontend.py
+++ mopidy_eboplayer2/frontend.py
@@ -16,16 +16,18 @@ class EboPlayerFrontend(pykka.ThreadingActor, core.CoreListener):
         logger.info("STARTING....")
         volume = Storage.get('volume', 50)
         self.core.mixer.set_volume(volume)
+        Storage.add_empty_title()
         return None
 
     def stream_title_changed(self, title: str) -> None:
         logger.info(f"Stream title: {title}")
         if Storage.write_title(title):
             lines = Storage.get_active_titles()
-            linesDict = {index: value for index, value in enumerate(lines)}
-            self.send('stream_history_changed', data=linesDict)
+            lines_dict = {index: value for index, value in enumerate(lines)}
+            self.send('stream_history_changed', data=lines_dict)
 
-    def volume_changed(self, volume):
+    @staticmethod
+    def volume_changed(volume):
         logger.info("Volume changed. Saving to settings file.")
         Storage.save('volume', volume)
 
