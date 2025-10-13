21160256ee2bd6974f7341da0cc04139c142cf87
Erik Bongers <mail@erikbongers.net>
2025-10-13 23:01:18 +0200

    ProgressBar with active and button properties.

--- typescript/components/eboProgressBar.ts
+++ typescript/components/eboProgressBar.ts
@@ -1,17 +1,69 @@
+import {console_yellow} from "../gui";
+
 export class EboProgressBar extends HTMLElement {
     private shadow: ShadowRoot;
     // noinspection JSUnusedGlobalSymbols
-    static observedAttributes = ["position", "min", "max", "button"];
+    static observedAttributes = ["position", "min", "max", "button", "active"];
     private position: number = 50;
     private min: number = 0;
     private max: number = 100;
+    private active: boolean = false;
     private button: boolean = true;
+    private styleTemplate: HTMLTemplateElement;
+    private divTemplate: HTMLTemplateElement;
+
     constructor() {
         super();
-        // let template = document.getElementById("history-line") as HTMLTemplateElement;
-        // let content = template.content;
+        console_yellow("Constructed");
+        this.styleTemplate = document.createElement("template");
+        // noinspection CssUnresolvedCustomProperty
+        this.styleTemplate.innerHTML = `
+<style>
+.movingGradient {
+    background-color: #555;
+}
+.active .movingGradient {
+    --darkGradient: #555;
+    background: linear-gradient(to right, var(--darkGradient),
+    #2a84a5, var(--darkGradient), #ffffff,
+        var(--darkGradient), #ca497c, var(--darkGradient), #9e9c2d, var(--darkGradient), #ee7752, var(--darkGradient),
+        #2a84a5
+    );
+    background-size: 1100% 1100%;  /*Must be a multiple of the number of colors above for a smooth transistion and the last color must be the first*/
+    animation: gradient 15s forwards infinite linear;
+    animation-timing-function: linear;
+    animation-direction: normal;
+}
+.button #button {
+        background-color: white;
+        width: 3px; 
+        display: inline-block;
+        box-shadow: 0 0 5px white, 0 0 5px white,  0 0 5px white, 0 0 5px white, 0 0 15px white;
+}
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    100% {
+        background-position: 100% 50%;
+    }
+}
+</style>`;
+        this.divTemplate = document.createElement("template");
+        this.divTemplate.innerHTML = `
+<div style="
+    background-color: #444; 
+    height: 1px; 
+    display: flex; 
+    ">
+    <div class="movingGradient" style="
+        height: 1px;
+        display: inline-block;">
+    </div>
+    <div id="button"></div>
+</div>
+`;
         this.shadow = this.attachShadow({mode: "open"});
-        // this.root.appendChild(content.cloneNode(true));
 
         this.render();
     }
@@ -23,76 +75,47 @@ export class EboProgressBar extends HTMLElement {
             case "min":
             case "max":
                 let test = parseInt(newValue);
-                if(isNaN(test))
+                if (isNaN(test))
                     throw `"${name}" attribute should be a number. Current value: "${newValue}"`;
                 this[name] = test;
                 break;
+            case "active":
             case "button":
-                if(!["true", "false"].includes(newValue))
+                if (!["true", "false"].includes(newValue))
                     throw `"${name}" attribute should be "true" or "false". Current value: "${newValue}"`;
-                this.button = newValue == "true";
+                this[name] = newValue == "true";
                 break;
         }
         if(!(this.min <= this.position && this.position <= this.max))
             throw `Attribute position="${this.position}" should be between min="${this.min}" and max="${this.max}".`;
-        this[name] = newValue;
         this.render();
         }
 
+    // noinspection JSUnusedGlobalSymbols
     connectedCallback() {
+        console_yellow("Connected");
     }
 
 
     render() {
-        const style = document.createElement("style");
-        style.textContent = EboProgressBar.styleText;
         let percent = (this.position - this.min) / (this.max-this.min) * 100;
-        this.shadow.innerHTML = `
-    <div style="
-        background-color: #444; 
-        height: 1px; 
-        display: flex; 
-        margin: 10px;">
-        <div class="movingGradient" style="
-            width: ${percent}%;
-            height: 1px;
-            display: inline-block;">
-        </div>
-        <div style="
-            background-color: white;
-            width: 3px; 
-            display: inline-block;
-            box-shadow: 0 0 5px white, 0 0 5px white,  0 0 5px white, 0 0 5px white, 0 0 15px white;">
-        </div>
-    </div>
-`;
-        this.shadow.appendChild(style);
+        this.shadow.innerHTML="";
+        this.shadow.appendChild(this.styleTemplate.content.cloneNode(true));
+        let styleElement = this.shadow.appendChild(document.createElement("style"));
+        styleElement.innerHTML = `.movingGradient { width: ${percent}%; } `;
+        let fragment = this.divTemplate.content.cloneNode(true) as DocumentFragment;
+        this.setClassFromBoolAttribute("button", fragment.firstElementChild as HTMLElement);
+        this.setClassFromBoolAttribute("active", fragment.firstElementChild as HTMLElement);
+        this.shadow.appendChild(fragment);
     }
     static define() {
         customElements.define("ebo-progressbar", EboProgressBar);
     }
 
-    static styleText = `
-.movingGradient {
-    --darkGradient: #555;
-    background: linear-gradient(to right, var(--darkGradient),
-    #2a84a5, var(--darkGradient), #ffffff,
-        var(--darkGradient), #872f51, var(--darkGradient), #757421, var(--darkGradient), #ee7752, var(--darkGradient),
-        #2a84a5
-    );
-    background-size: 1100% 1100%;  /*Must be a multiple of the number of colors above for a smooth transistion and the last color must be the first*/
-    animation: gradient 15s forwards infinite linear;
-    animation-timing-function: linear;
-    animation-direction: normal;
-}
-
-@keyframes gradient {
-    0% {
-        background-position: 0% 50%;
+    setClassFromBoolAttribute(attName: string, el: HTMLElement) {
+        if (this[attName] == true)
+            el.classList.add(attName);
+        else
+            el.classList.remove(attName);
     }
-    100% {
-        background-position: 100% 50%;
-    }
-}
-    `;
 }
