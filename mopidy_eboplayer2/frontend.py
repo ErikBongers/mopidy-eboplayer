64b3fa7d24d47fbe4ab234f2d130c09a5b46bc18
Erik Bongers <mail@erikbongers.net>
2026-01-17 21:25:10 +0100

    Test python curl from front to backend.

--- mopidy_eboplayer2/frontend.py
+++ mopidy_eboplayer2/frontend.py
@@ -2,7 +2,7 @@ import logging
 import pykka
 import json
 from mopidy import core
-
+import urllib.request
 from mopidy_eboplayer2.Storage import Storage
 from mopidy_eboplayer2.webSocketHandler import broadcast
 
@@ -24,10 +24,11 @@ class EboPlayerFrontend(pykka.ThreadingActor, core.CoreListener):
         self.core.mixer.set_volume(volume)
 
     def track_playback_started(self, tl_track):
-        logger.info("STARTED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        logger.info(tl_track.track.uri)
         self.current_track_uri = tl_track.track.uri
         self.storage.switch_stream_uri(self.current_track_uri)
+        contents = urllib.request.urlopen("http://192.168.1.111:6680/eboback/data/get_excluded_streamlines?uri=eboback:stream:http://icecast.vrtcdn.be/klara-high.mp3").read()
+        logger.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"+contents)
+
 
     def track_playback_ended(self, tl_track, time_position):
         self.on_track_ended()
