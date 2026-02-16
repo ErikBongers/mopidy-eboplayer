469373bdb5d3b8110978fff427d010acb4683365
Erik Bongers <mail@erikbongers.net>
2026-02-16 22:31:47 +0100

    GenreDefs: version with divs, no grouping yet.

--- mopidy_eboplayer2/www/typescript/components/eboGenresComp.ts
+++ mopidy_eboplayer2/www/typescript/components/eboGenresComp.ts
@@ -33,38 +33,49 @@ export class EboGenresComp extends EboComponent {
                 overflow-y: auto;
                 flex-grow: 1;
             }
-            h1, h2, h3, h4, h5, h6 {
+            .lvl1, .lvl2, .lvl3, .lvl4, .lvl5, .lvl6 {
                 margin: 0;
                 padding: 0;
                 font-size: 1rem;
                 font-weight: normal;
                 text-align: left;
             }
-            h1 {
+            .lvl1 {
                 font-size: 1.2rem;
                 font-weight: bold;
                 margin-inline-start: 1rem;
             }
-            h2 {
+            .lvl2 {
                 font-size: 1.1rem;
                 font-weight: bold;
                 margin-inline-start: 2rem;
             }
-            h3 {
+            .lvl3 {
                 font-size: 1rem;
                 margin-inline-start: 3rem;
             }
-            h4 {
+            .lvl4 {
                 font-size: .9rem;
                 margin-inline-start: 4rem;
             }
-            h5 {
+            .lvl5 {
                 font-size: .8rem;
                 margin-inline-start: 5rem;
             }
-            h6 {
+            .lvl6 {
                 font-size: .7rem;
                 margin-inline-start: 6rem;
+            }
+            .hasChildren::before {
+                content: "\\25B6";
+                color: white;
+                position: relative;
+                left: -1rem;
+                float: left;
+                width: 0;
+                height: 1rem;
+                cursor: pointer;
+                font-size: .8rem;
             }
         </style>
         `;
@@ -92,20 +103,25 @@ export class EboGenresComp extends EboComponent {
 
     override update(shadow:ShadowRoot) {
         let container = shadow.getElementById("scrollContainer") as HTMLElement;
-        this.genreDefs.forEach(genreDef => {
-            this.renderGenreDef(container, genreDef);
+        this.genreDefs.forEach((genreDef, index) => {
+            if(this.genreDefs.length > index + 1)
+                this.renderGenreDef(container, genreDef, this.genreDefs[index+1]);
+            else
+                this.renderGenreDef(container, genreDef, null);
         });
     }
 
-    private renderGenreDef(container: HTMLElement, genreDef: GenreDef) {
+    private renderGenreDef(container: HTMLElement, genreDef: GenreDef, nextGenreDef: GenreDef | null) {
         let lineDiv = document.createElement("div");
         container.appendChild(lineDiv);
+        if(nextGenreDef?.name == genreDef.child)
+            lineDiv.classList.add("hasChildren");
         let name = genreDef.name;
-        if(genreDef.child)
+        if(genreDef.child) {
             name = genreDef.child;
-        let h = "h"+(genreDef.level+1);
-        let lineElement = document.createElement(h) as HTMLHeadingElement;
-        lineDiv.appendChild(lineElement);
-        lineElement.textContent = name;
+        }
+        lineDiv.textContent = name;
+        let lvl = "lvl"+(genreDef.level+1);
+        lineDiv.classList.add(lvl);
     }
 }
